7fb0df905944d1455455b497e1d243c8b6ab7c8c
your-SamGreenwood <your-sagg_84@hotmail.com>
2023-12-14 15:53:08 -0400

    added new API key, added to README and labelled index3

--- README.md
+++ README.md
@@ -4,10 +4,28 @@
 
 by Emilia Stewart, Ethan Wright, Shawn Meister & Sam Greenwood
 
+## User Story
+
+AS A busy person who loves Christmas 
+
+I WANT to take a fun short quiz to help choose my next Christmas Movie 
+
+SO THAT I can save myself and my family time and effort this season
+
 ## Usage
 
-
-## User Story
+GIVEN I am taking a quiz
+WHEN I Click Let's Get Started button 
+THEN first question modal will populate
+WHEN I make my selection(s)
+THEN five more question modals will populate after clicking forward arrow each time 
+WHEN I do not make a selection 
+THEN I will be altered at the end of the quiz to go back and make a selection
+WHEN all the selections are made and the Submit button
+THEN my next Christmas Movie will be revealed on the next page
+WHEN I have reviewed my selection movie card with Image, Title, Description, Stream Availibility, Rating and Release Date
+THEN I will  be able to click Next Movie to review other selections
+within the chosen criteria or click  to redo the quiz
 
 
 ## Mockup
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,14 +1,14 @@
-const url = 'https://streaming-availability.p.rapidapi.com/genres';
+const url = 'https://streaming-availability.p.rapidapi.com/search/filters?country=us&services=netflix,prime,hbo,hulu&show_type=movie&genre=16,35,10751&keyword=Christmas&year_max=1967';
 
 const options = {
-method: 'GET', 
-headers: {
-    'Content-Type': 'application/json',
-    'X-RapidAPI-Key': 'df70185a90mshe979c7904560fe7p11849fjsn2041343439ed',
-    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-}
-};
-fetch(url, options)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error', error));
+    method: 'GET', 
+    headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': '2e0c500a89mshab8f861fe70bf86p1a5cdfjsn898ddcd83248',
+       'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+   }
+    };
+    fetch(url, options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error', error));
