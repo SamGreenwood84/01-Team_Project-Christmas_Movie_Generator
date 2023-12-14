74cdb4cc9ec8da26d644432a82e7704c510996c8
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2023-12-14 18:51:13 -0400

    starter code for streaming API

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,3 +1,9 @@
+let genreId;
+let year;
+let yearMin = `&year_min=${year}`;
+let yearMax = `&year_max=${year}`;
+let firstGenreId = `genre=${genreId}`;
+let nextGenreId = `,${genreId}`;
 
 const url = 'https://streaming-availability.p.rapidapi.com/search/filters?country=us&services=netflix,prime,hbo,hulu&show_type=movie&genre=16,35,10751&keyword=Christmas&year_max=1967';
 
@@ -11,5 +17,13 @@ const options = {
     };
     fetch(url, options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            let results = data.result;
+            results.forEach(i => {
+                console.log(results[i].streamingInfo.us[0].service);
+            });
+            
+        }
+        )
         .catch(error => console.error('Error', error));
