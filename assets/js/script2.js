eed5a51d6cd79885ebb7b2b4604a9144fad393c6
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2023-12-14 16:23:56 -0400

    custom alert box made for button presses

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,10 +1,14 @@
+<<<<<<< Updated upstream
 const url = 'https://streaming-availability.p.rapidapi.com/countries';
+=======
+const url = 'https://streaming-availability.p.rapidapi.com/search/filters?country=us&services=netflix,prime,hbo,hulu&show_type=movie&genre=16,35,10751&keyword=Christmas&year_max=1967';
+>>>>>>> Stashed changes
 
 const options = {
 method: 'GET', 
 headers: {
     'Content-Type': 'application/json',
-    'X-RapidAPI-Key': 'df70185a90mshe979c7904560fe7p11849fjsn2041343439ed',
+    // 'X-RapidAPI-Key': 'df70185a90mshe979c7904560fe7p11849fjsn2041343439ed',
     'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
 }
 };
