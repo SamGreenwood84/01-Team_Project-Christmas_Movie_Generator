53ac04e11cecf02e9eb4c0a5910a441f1164bd32
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2023-12-14 21:29:56 -0400

    added link to live app in README, more code for Stream API

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -2,10 +2,15 @@
 let myMovie = JSON.parse(localStorage.getItem("chosenMovie"));
 let myArray = JSON.parse(localStorage.getItem("ratedMovies"));
 let title = "";
+let movieId = "";
+let chosenMovie = "";
 
 // Event Listeners
 $(document).ready(() => {
   title = myMovie.title;
+  console.log(title);
+  movieId = myMovie.id;
+  console.log(movieId);
   populateMovieCard();
 });
 
@@ -13,17 +18,48 @@ $(".submit").on("click", () => {
   pickRandomMovie();
   function pickRandomMovie() {
     let index = Math.floor(Math.random() * myArray.length);
-    let chosenMovie = myArray[index];
+    chosenMovie = myArray[index];
     title = chosenMovie.title;
     populateMovieCard();
+    getStream(title);
   }
 });
 $(".restart").on("click", () => {
     window.location.href = "index.html";
 });
 
+function getStream(title) {
+  movieId = chosenMovie.id;
+  console.log(title);
+  const url = `https://streaming-availability.p.rapidapi.com/search/filters?country=us&services=netflix,prime,hbo,hulu&show_type=movie&keyword=${title}`;
+
+  const options = {
+      method: 'GET', 
+      headers: {
+          'Content-Type': 'application/json',
+          'X-RapidAPI-Key': 'ec3251b884msh32e86fe3f91ca4fp11e3b2jsnf9ee270a236c',
+          // 'X-RapidAPI-Key': '2e0c500a89mshab8f861fe70bf86p1a5cdfjsn898ddcd83248',
+         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+     }
+      };
+      fetch(url, options)
+          .then(res => res.json())
+          .then(data => {
+              console.log(data);
+              let results = data.result;
+              
+              console.log(results[0].streamingInfo.us[0].service);
+              
+              // results[0].tmdbId can be matched with chosenMovie ID
+          }
+          )
+          .catch(error => console.error('Error', error));
+}
+
+
 // Movie Card
 function populateMovieCard() {
+
   const options = {
     method: "GET",
     headers: {
